98b34e6769ce7072c4ae1f4a375d29c87a94dd44
Mohit Kumar <mohitk02082000@gmail.com>
2021-02-07 15:13:34 +0530

    implement signup functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
 	return (
+		<AuthProvider>
 		<Container
 			className="d-flex align-items-center justify-content-center"
 			style={{ minHeight: "100vh" }}
@@ -11,6 +13,9 @@ function App() {
 				<Signup />
 			</div>
 		</Container>
+
+		</AuthProvider>
+
 	);
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,16 +1,43 @@
-import React,{useRef} from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Signup() {
-	const emailRef = useRef()
-	const passwordRef = useRef()
-	const passwordConfirmRef = useRef()
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
+
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	const { signup,currentUser } = useAuth();
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError("passwords do not match");
+		}
+
+		try {
+			setError("");
+			setLoading(true);
+			await signup(emailRef.current.value, passwordRef.current.value);
+		} catch {
+			setError("Failed to create an account.");
+		} finally {
+			setLoading(false);
+		}
+	}
+
 	return (
 		<>
 			<Card>
 				<Card.Body>
 					<h2 className="text-center mt-4">Sign Up</h2>
-					<Form>
+					{error && <Alert variant="danger">{error}</Alert>}
+					{ currentUser.email }
+					<Form onSubmit={handleSubmit}>
 						<Form.Group id="email">
 							<Form.Label>Email</Form.Label>
 							<Form.Control type="email" ref={emailRef} required />
@@ -21,9 +48,9 @@ export default function Signup() {
 						</Form.Group>
 						<Form.Group id="passwod-confirm">
 							<Form.Label>Password Confirmation</Form.Label>
-							<Form.Control type="email" ref={passwordConfirmRef} required />
+							<Form.Control type="password" ref={passwordConfirmRef} required />
 						</Form.Group>
-						<Button className="w-100" type="submit">
+						<Button disabled={loading} className="w-100" type="submit">
 							Sign Up
 						</Button>
 					</Form>
